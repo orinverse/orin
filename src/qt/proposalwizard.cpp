77fd5d09714f41f51495dde599526f7a5073816b
thephez <thephez@users.noreply.github.com>
2025-11-13 10:27:11 -0500

    feat: automatically validate proposal data when review screen loads

--- src/qt/proposalwizard.cpp
+++ src/qt/proposalwizard.cpp
@@ -199,6 +199,7 @@ void ProposalWizard::onNextFromDetails()
 {
     buildJsonAndHex();
     m_ui->stackedWidget->setCurrentIndex(1);
+    onValidateJson();  // Automatically validate when entering the review page
 }
 
 void ProposalWizard::onBackToDetails() { m_ui->stackedWidget->setCurrentIndex(0); }
